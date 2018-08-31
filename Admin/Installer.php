ad07515396728673b3a0b68283ecf3c991be9207
Dennis Eichhorn <spl1nes.com@googlemail.com>
2018-08-31 21:14:53 +0200

    improve unit tests

---
+++ Admin/Installer.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 7.2
+ *
+ * @package    Modules\TestModule\Admin
+ * @copyright  Dennis Eichhorn
+ * @license    OMS License 1.0
+ * @version    1.0.0
+ * @link       http://website.orange-management.de
+ */
+declare(strict_types=1);
+
+namespace Modules\TestModule\Admin;
+
+use phpOMS\DataStorage\Database\DatabasePool;
+use phpOMS\Module\InstallerAbstract;
+use phpOMS\Module\InfoManager;
+
+/**
+ * Accounts payable install class.
+ *
+ * @package    Modules\TestModule\Admin
+ * @license    OMS License 1.0
+ * @link       http://website.orange-management.de
+ * @since      1.0.0
+ */
+class Installer extends InstallerAbstract
+{
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function install(DatabasePool $dbPool, InfoManager $info) : void
+    {
+        parent::install($dbPool, $info);
+    }
+}
