224ce4ada20a2b3f3f4672c153c2d84234032526
Dennis Eichhorn <spl1nes.com@googlemail.com>
2021-03-05 21:08:46 +0100

    draft additional languages

---
+++ Theme/Backend/Lang/cs.lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Orange Management
+ *
+ * PHP Version 8.0
+ *
+ * @package   Modules\Localization
+ * @copyright Dennis Eichhorn
+ * @license   OMS License 1.0
+ * @version   1.0.0
+ * @link      https://orange-management.org
+ */
+declare(strict_types=1);
+
+return ['TestModule' => [
+    'Test1' => 'test1',
+    'Test2' => 'test2',
+]];
